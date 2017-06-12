b23de8e4a50ad37a35a86eed6fa8aaeaa40b539a
Struble,Chris <cstruble@vertafore.com>
2017-06-12 13:54:08 -0700

    Initial commit

---
+++ C#/CheckConverter.Tests/UnitTest1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CheckConverter.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void ConvertSingleDigitTest()
+        {
+            Assert.AreEqual(CheckConverter.Program.ConvertToWords(7.ToString()), "Seven Only");
+        }
+
+        [TestMethod]
+        public void ConvertMultiDigitTest()
+        {
+            Assert.AreEqual(CheckConverter.Program.ConvertToWords(2683.ToString()), "Two Thousand Six Hundred Eighty Three Only");
+        }
+
+        [TestMethod]
+        public void ConvertInvalidDigitTest()
+        {
+            Assert.AreEqual(CheckConverter.Program.ConvertToWords("Eleventy One"), " Only");
+        }
+    }
+}
